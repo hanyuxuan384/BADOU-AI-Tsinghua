d4551dd333cbbc14ccbfdcaa108bed1c92ad7fc1
hanyuxuan384 <78011964+hanyuxuan384@users.noreply.github.com>
2021-12-18 11:02:53 +0800

    Update gray.py

--- 11-韩雨轩-福建/第二周作业/gray.py
+++ 11-韩雨轩-福建/第二周作业/gray.py
@@ -8,19 +8,19 @@ img=cv2.imread("lenna.png")  #读取图像
 #显示cv读取的图像
 plt.subplot(221)
 plt.imshow(img)
-plt.show()
+
 #显示GBR改为RGB后的三通道灰度图
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.subplot(222)
 plt.imshow(img_gray)  #plt显示图像默认三通道，不是单灰
 #plt.axis('off')
-plt.show()
+
 #显示灰度图
 plt.subplot(223)
 plt.imshow(img_gray, cmap='gray')
 
 '''二值化'''
-img_binary = np.where(img_gray >= 0.5, 1, 0)
+img_binary = np.where(img_gray >= 127, 255, 0)
 plt.subplot(224)
 plt.imshow(img_binary, cmap='gray')
 plt.show()
